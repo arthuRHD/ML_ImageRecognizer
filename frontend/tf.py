90293810e79b4fa5843d387f3f15ea39b75d05fe
Arthur RICHARD <arthur.richard@protonmail.com>
2020-11-11 18:56:19 +0100

    flower tensor training test

--- frontend/tf.py
+++ frontend/tf.py
@@ -1,16 +1,94 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
 import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
-# with open("../media/images/cesi.png", "rb") as image:
-ds = tfds.load('mnist', split="train", shuffle_files=True)
+import pathlib
 
-ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
-for example in ds.take(1):
-    image, label = example["image"], example["label"]
-    print(image, label)
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(origin=dataset_url, 
+                                   fname='flower_photos', 
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image)) 
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+num_classes = 5
+
+model = tf.keras.Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
